b8a8e68a405af50893ea2697f0035d26c04f8ee5
Vitali Pilipchuk <cpaaaik@gmail.com>
2020-09-30 19:23:20 +0300

    feat: start .avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (arguments.length) {
+        let minNum = 0;
+
+        for (let i of array) {
+            if (i < minNum) {
+                minNum = i;
+            }
+        }
+        return minNum
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (arguments.length) {
+        let maxNum = 0;
+
+        for (let i of array) {
+            if (i > maxNum) {
+                maxNum = i;
+            }
+        }
+        return maxNum
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+   return  array.reduce((a, b) => a + b) / array.length
 }
+
+
+
